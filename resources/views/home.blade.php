b6ef0e3a689da40b4ef671bc50306a9dd059934c
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-09-21 22:39:26 -0500

    Working with the main page after log in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div clas="col-3 p-5">
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" class="rounded-circle" style="height: 100px;">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        </div>
+        <div clas="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="px-5"><strong>153</strong> posts</div>
+                <div class="px-5"><strong>2.5</strong> followers</div>
+                <div class="px-5"><strong>212</strong> following</div>
             </div>
         </div>
     </div>
