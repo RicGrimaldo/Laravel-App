f1d8baf593b8b6d1aa710645fbef5ba501c5ac17
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-09-25 18:49:49 -0500

    Post method added

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,7 @@ Auth::routes();
 
 Route::get('/p/create', 'App\Http\Controllers\PostsController@create');
 
+Route::post('/p', 'App\Http\Controllers\PostsController@store');
+
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
