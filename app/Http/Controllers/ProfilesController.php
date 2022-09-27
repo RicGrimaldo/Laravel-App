f6292a6c7fc3dd063b761277637849643f989f12
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-09-27 13:22:48 -0500

    Refactoring even more the ProfilesController

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,13 +7,13 @@ use App\Models\User;
 
 class ProfilesController extends Controller
 {
-    public function index(\App\Models\User $user)
+    public function index(User $user)
     {
         return view('profiles.index', compact('user'));
     }
 
-    public function edit(\App\Models\User $user){
-
+    public function edit(User $user){
+        return view('profiles.edit', compact('user'));
     }
 
 }
